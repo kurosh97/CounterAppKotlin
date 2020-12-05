09a742f5fa8d51aca99e64df75d2e4cd422d0e5d
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2020-12-05 22:26:54 +0200

    reset, decrease btn added

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -11,15 +11,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myBtn = findViewById(R.id.button) as Button
+        val addBtn = findViewById(R.id.addBtn) as Button
+        val resetBtn = findViewById(R.id.resetBtn) as Button
+        val minusBtn = findViewById(R.id.resetBtn) as Button
+
         val myTextView = findViewById(R.id.textView) as TextView
         var timesClicked = 0
 
-        myBtn.setOnClickListener {
+        addBtn.setOnClickListener {
+            timesClicked++
             myTextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "1+", Toast.LENGTH_SHORT).show()
-            timesClicked ++
+
         }
+
+
+        resetBtn.setOnClickListener{
+            timesClicked = 0
+            myTextView.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "Reset", Toast.LENGTH_SHORT).show()
+        }
+
+        minusBtn.setOnClickListener {
+            timesClicked -= 1
+            myTextView.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "-1", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
