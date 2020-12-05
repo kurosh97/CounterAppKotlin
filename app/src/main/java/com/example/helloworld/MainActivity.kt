c5263ec2f94593255d26cfc2eb4bee52181d00bb
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2020-12-05 18:48:05 +0200

    got reference to button & set on-Click listener

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myBtn = findViewById(R.id.button) as Button
+        val myTextView = findViewById(R.id.textView) as TextView
+
+        myBtn.setOnClickListener {
+            myTextView.text = "Hello mate"
+            Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
