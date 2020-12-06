a7c20f17b73506d465264f98a95a45f2fbc2ef09
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2020-12-06 15:33:41 +0200

    Maximum capacity featur added

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,29 +12,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addBtn = findViewById(R.id.addBtn) as Button
-        val resetBtn = findViewById(R.id.resetBtn) as Button
-        val minusBtn = findViewById(R.id.resetBtn) as Button
+        val myTextView = findViewById<TextView>(R.id.textView)
 
-        val myTextView = findViewById(R.id.textView) as TextView
+        val addBtn = findViewById<Button>(R.id.addBtn)
+        val resetBtn = findViewById<Button>(R.id.resetBtn)
+        val minusBtn = findViewById<Button>(R.id.minusBtn)
+        val enterBtn = findViewById<Button>(R.id.enterBtn)
+        
         var timesClicked = 0
+        var maxValue = 0
 
-        addBtn.setOnClickListener {
-            timesClicked++
-            myTextView.text = timesClicked.toString()
-            Toast.makeText(this@MainActivity, "1+", Toast.LENGTH_SHORT).show()
+        val maximumEnteryEditView = findViewById<EditText>(R.id.maximumEntryED)
 
+        enterBtn.setOnClickListener {
+            maxValue = maximumEnteryEditView.text.toString().toInt()
         }
 
 
-        resetBtn.setOnClickListener{
+        addBtn.setOnClickListener {
+          if (timesClicked < maxValue) {
+              timesClicked++
+              myTextView.text = timesClicked.toString()
+              Toast.makeText(this@MainActivity, "1+", Toast.LENGTH_SHORT).show()
+          }else if (timesClicked >= maxValue){
+              timesClicked = maxValue
+              Toast.makeText(this@MainActivity, "Maximum capacity reached!!!", Toast.LENGTH_LONG).show()
+          }
+          }
+
+
+        resetBtn.setOnClickListener {
             timesClicked = 0
             myTextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "Reset", Toast.LENGTH_SHORT).show()
         }
 
         minusBtn.setOnClickListener {
-            timesClicked -= 1
+            if (timesClicked > 0) {
+                timesClicked -= 1
+            }
             myTextView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity, "-1", Toast.LENGTH_SHORT).show()
         }
